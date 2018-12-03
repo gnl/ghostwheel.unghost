dd16de349ec337eb93cd46e347e29e760047793d
George Lipov <gnl@lipov.net>
2018-12-03 02:55:13 +0100

    Synchronise Ghostwheel module versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gnl/ghostwheel.unghost "0.3.1-SNAPSHOT"
+(defproject gnl/ghostwheel.unghost "0.3.5-SNAPSHOT"
   :description "A tiny library used to remove Ghostwheel code in production"
   :url "https://github.com/gnl/ghostwheel.unghost"
   :scm {:name "git"
